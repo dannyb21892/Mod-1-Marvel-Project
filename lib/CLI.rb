2e167926a94f0699fbce1479675583e656d5df26
github username <github email address>
2018-03-07 17:22:37 -0500

    Added questions, output from CLI

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,4 +1,6 @@
+require 'pry'
 def welcome
+
   options = {"1" => "characters",
     "2" => "creators",
     "3" => "events",
@@ -10,12 +12,57 @@ def welcome
   puts "Welcome to the Marvel recommendation app."
   puts "Please choose which kind of object to search for: \n1: Characters\n2: Creators\n3: Events"
   type = gets.chomp
+  #binding.pry
   while !options.keys.include?(type)
     "Please type 1, 2, or 3 and press return."
     type = gets.chomp
   end
+  #binding.pry
   "aeiou".split('').include?(options[type][0]) ? thingy = "n" : thingy = ""
   puts "Please enter a#{thingy} #{options[type][0..-2]} to search for:"
   search_term = gets.chomp
   [options[type], search_term]
+end
+
+def information_request(user_input)
+  if user_input[0] == "characters"
+    puts "\n"
+    puts "Please select the number of a question:"
+    puts "1. What were the major Marvel Universe events that #{user_input[1]} participated in?"
+    choice = gets.chomp
+    choices = {"1" => "events"}
+    return choices[choice]
+  elsif user_input[0] == "events"
+    puts "\n"
+    puts "Please select the number of a question:"
+    puts "1. Which Marvel creators worked on #{user_input[1]}?"
+    puts "2. Which Marvel characters participated in #{user_input[1]}?"
+    choice = gets.chomp
+    choices = {"1" => "creators", "2" => "characters"}
+    return choices[choice]
+  elsif user_input[0]== "creators"
+    puts "\n"
+    puts "Please select the number of a question:"
+    puts "1. Which Marvel events did #{user_input[1]} work on?"
+    choice = gets.chomp
+    choices = {"1" => "events"}
+    return choices[choice]
+  else
+    puts "Invalid input."
+  end
+end
+
+def return_info(user_input, type_of_response)
+  relationship_hash = {
+    "characters" => {"events" => "#{user_input[1]} participated in the following events:"},
+    "events" => {"creators" => "#{user_input[1]} was worked on by the following creators:",
+    "characters" => "#{user_input[1]} featured the following characters:"},
+    "creators" => {"events" => "#{user_input[1]} worked on the following events:"}
+  }
+  puts relationship_hash[user_input[0]][type_of_response]
+  class_hash = {"characters" => Character, "creators" => Creator, "events" => Event}
+  array = class_hash[type_of_response].all.map do |object|
+    object.name
+  end
+  puts array
 end
